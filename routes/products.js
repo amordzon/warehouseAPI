819bd23fb3978568403b3b5d1dd0d8d9e6fb4491
Aleksandra Mordzon <aleksandramordzon@Aleksandras-MacBook-Air.local>
2023-01-15 19:15:45 +0100

    init

---
+++ routes/products.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const recordRoutes = express.Router();
+const dbo = require("../db/conn");
+const ObjectId = require("mongodb").ObjectId;
+
+recordRoutes.route("/products").get(function (req, res) {
+  let db_connect = dbo.getDb("warehouse");
+  db_connect
+    .collection("products")
+    .find({})
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+module.exports = recordRoutes;
